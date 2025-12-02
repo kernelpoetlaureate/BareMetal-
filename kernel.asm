8266cec579536d3a06de3dc6b0f116198610a983
Giorgi <giorgitchankvetadze1997@gmail.com>
2025-12-02 15:53:01 +0400

    rewriting nasm into manual version

--- kernel.asm
+++ kernel.asm
@@ -1,103 +1,206 @@
 [bits 32]
 [org 0x1000]
 
-; Entry point
 global _start
+
 _start:
-    mov ax, 0x10          ; Setup data segment
+    ; --- 1. Setup Segments ---
+    mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov esp, stack_top    ; Setup stack
-    
-    call clear_screen
-    call print_msg        ; Print "Kernel started!"
-    call create_tasks     ; Create 3 tasks
-    call run_tasks        ; Run them all
-    
-    jmp $                 ; Hang
+    mov esp, stack_top
 
-; ========== TASK MANAGEMENT ==========
+    ; --- 2. Manual Call to clear_screen ---
+    ; MECHANIC: A 'call' is just Pushing return address + Jumping
+    sub esp, 4                  ; 1. Grow stack down by 4 bytes
+    mov dword [esp], .ret_1     ; 2. Save the address of label .ret_1 to stack
+    jmp clear_screen            ; 3. Jump to function
+.ret_1:                         ; 4. This is where we come back
+
+    ; --- 3. Manual Call to print_msg ---
+    sub esp, 4
+    mov dword [esp], .ret_2
+    jmp print_msg
+.ret_2:
+
+    ; --- 4. Manual Call to create_tasks ---
+    sub esp, 4
+    mov dword [esp], .ret_3
+    jmp create_tasks
+.ret_3:
+
+    ; --- 5. Manual Call to run_tasks ---
+    sub esp, 4
+    mov dword [esp], .ret_4
+    jmp run_tasks
+.ret_4:
+
+    jmp $                       ; Hang
+
+; ==========================================================
+; TASK MANAGEMENT
+; ==========================================================
 
 create_tasks:
-    mov eax, task_a       ; Create task A
-    call add_task
-    mov eax, task_b       ; Create task B
-    call add_task
-    mov eax, task_c       ; Create task C
-    call add_task
-    ret
+    ; Task A
+    mov eax, task_a
+    ; Manual call add_task
+    sub esp, 4
+    mov dword [esp], .ct_ret1
+    jmp add_task
+.ct_ret1:
 
-; Add task to process table
-; Input: EAX = function address
+    ; Task B
+    mov eax, task_b
+    sub esp, 4
+    mov dword [esp], .ct_ret2
+    jmp add_task
+.ct_ret2:
+
+    ; Task C
+    mov eax, task_c
+    sub esp, 4
+    mov dword [esp], .ct_ret3
+    jmp add_task
+.ct_ret3:
+
+    ; Manual RET
+    mov ebx, [esp]      ; 1. Load return address from stack into EBX
+    add esp, 4          ; 2. Shrink stack (pop)
+    jmp ebx             ; 3. Jump back to caller
+
+; ----------------------------------------------------------
+; Add task (Input: EAX = function address)
 add_task:
     mov edi, [task_count]
-    shl edi, 2            ; Multiply by 4 (each entry is a dword)
-    add edi, tasks
-    mov [edi], eax        ; Store function pointer
+    shl edi, 2          ; Multiply index by 4
+    add edi, tasks      ; Add base address
+    mov [edi], eax      ; Store pointer
     inc dword [task_count]
-    ret
 
-; Run all tasks once
+    ; Manual RET
+    mov ebx, [esp]      ; Read return address
+    add esp, 4          ; Clear stack slot
+    jmp ebx             ; Go back
+
+; ----------------------------------------------------------
+; Run tasks
 run_tasks:
-    xor ebx, ebx          ; Counter = 0
+    xor ebx, ebx        ; Counter = 0
 .loop:
     cmp ebx, [task_count]
-    jge .done             ; If counter >= task_count, done
-    
-    mov eax, [tasks + ebx*4]  ; Get task function pointer
-    call eax              ; Run task
-    
+    jge .done
+
+    ; Calculate address of task pointer
+    mov edx, ebx        ; Copy index
+    shl edx, 2          ; Multiply by 4
+    add edx, tasks      ; Add base
+    mov eax, [edx]      ; EAX now holds the address of the function (e.g., task_a)
+
+    ; Manual CALL to a DYNAMIC address (e.g., call eax)
+    sub esp, 4              ; Make space
+    mov dword [esp], .back  ; Push return label
+    jmp eax                 ; Jump to the address inside EAX
+.back:
+
     inc ebx
     jmp .loop
+
 .done:
-    ret
+    ; Manual RET
+    mov ebx, [esp]
+    add esp, 4
+    jmp ebx
 
-; ========== TASKS ==========
-
+; ==========================================================
+; TASKS
+; ==========================================================
 task_a:
     mov esi, msg_a
-    call print
-    ret
+    ; Manual call print
+    sub esp, 4
+    mov dword [esp], .ret
+    jmp print
+.ret:
+    ; Manual ret
+    mov ebx, [esp]
+    add esp, 4
+    jmp ebx
 
 task_b:
     mov esi, msg_b
-    call print
-    ret
+    sub esp, 4
+    mov dword [esp], .ret
+    jmp print
+.ret:
+    mov ebx, [esp]
+    add esp, 4
+    jmp ebx
 
 task_c:
     mov esi, msg_c
-    call print
-    ret
+    sub esp, 4
+    mov dword [esp], .ret
+    jmp print
+.ret:
+    mov ebx, [esp]
+    add esp, 4
+    jmp ebx
 
-; ========== SCREEN OUTPUT ==========
+; ==========================================================
+; SCREEN OUTPUT (Rewritten to remove REP STOSW)
+; ==========================================================
 
 clear_screen:
     mov edi, 0xB8000
     mov ecx, 80*25
-    mov ax, 0x0720        ; Space with grey on black
-    rep stosw
-    ret
+    mov ax, 0x0720      ; Space + Color
+
+.cls_loop:
+    mov [edi], ax       ; Manual write to video memory
+    add edi, 2          ; Next character cell
+    dec ecx             ; Decrement counter
+    jnz .cls_loop       ; If not zero, loop
+
+    ; Manual RET
+    mov ebx, [esp]
+    add esp, 4
+    jmp ebx
 
 print_msg:
     mov esi, kernel_msg
-    ; Fall through to print
+    ; Fall through (Optimized jump)
 
 print:
     mov edi, [cursor]
-.loop:
-    lodsb                 ; Load byte from ESI
-    test al, al           ; Check for null terminator
-    jz .done
-    mov ah, 0x07          ; Grey on black
-    stosw                 ; Write to screen
-    jmp .loop
-.done:
+.print_loop:
+    ; Manual LODSB (Load byte at ESI to AL, inc ESI)
+    mov al, [esi]
+    inc esi
+    
+    test al, al
+    jz .print_done
+    
+    mov ah, 0x07        ; Color
+    
+    ; Manual STOSW (Store AX to EDI, inc EDI by 2)
+    mov [edi], ax
+    add edi, 2
+    
+    jmp .print_loop
+
+.print_done:
     mov [cursor], edi
-    ret
+    
+    ; Manual RET
+    mov ebx, [esp]
+    add esp, 4
+    jmp ebx
 
-; ========== DATA ==========
-
+; ==========================================================
+; DATA
+; ==========================================================
 kernel_msg db "Kernel started!", 0
 msg_a db "Task A running!", 0
 msg_b db "Task B running!", 0
@@ -105,10 +208,16 @@ msg_c db "Task C running!", 0
 
 cursor dd 0xB8000
 task_count dd 0
-tasks times 16 dd 0       ; Space for 16 task pointers
+tasks times 16 dd 0
 
-; ========== STACK ==========
+; ==========================================================
+; PADDING (Before BSS)
+; ==========================================================
+times (512 - (($ - $$) % 512)) db 0
 
+; ==========================================================
+; STACK
+; ==========================================================
 section .bss
     resb 4096
 stack_top:
